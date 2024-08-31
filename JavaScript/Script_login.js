230bc2d64c36f6c61ad3c506892ad2fb954658ea
JuanArias <ariaspulidojuan@gmail.com>
2024-08-30 23:02:48 -0500

    CREACION DE LA CLASE JS PARA LOGIN

---
+++ JavaScript/Script_login.js
@@ -0,0 +1,17 @@
+document.getElementById('registerForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const nombre = document.getElementById('nombre').value;
+    const apellidos = document.getElementById('apellidos').value;
+    const cedula = document.getElementById('cedula').value;
+    const celular = document.getElementById('celular').value;
+    
+    // Guardar los datos en localStorage
+    localStorage.setItem('nombre', nombre);
+    localStorage.setItem('apellidos', apellidos);
+    localStorage.setItem('cedula', cedula);
+    localStorage.setItem('celular', celular);
+    
+    alert('Registro exitoso');
+    window.location.href = 'index.html'; // Redirigir a la página principal
+});
