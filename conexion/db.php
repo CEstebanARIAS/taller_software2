66ecb7f862d01883a7127fb1ceb3252d140fcba5
Luis Silvestre <luissilvestre70@gmail.com>
2024-08-31 20:13:18 -0500

    Conexion

--- conexion/db.php
+++ conexion/db.php
@@ -3,12 +3,15 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "taller_software2";
+$port = 3307; // Especifica el puerto
 
 // Crear conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 // Verificar conexión
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
+
+echo "Conexión exitosa a la base de datos taller_software2 en el puerto 3307";
 ?>
